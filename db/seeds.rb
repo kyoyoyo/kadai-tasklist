330cae11ed56cf75c23655318d6597da5029cc89
kyoyoyo <k.yo@digitalsoft.co.jp>
2021-02-01 07:34:59 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sta = ["finished", "unfinished"]
+
+(1..100).each do |number|
+  Task.create(status: sta[rand(sta.length)], content: "task " + number.to_s)
+end
